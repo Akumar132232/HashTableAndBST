5dd9c300db86e53574f8c07f587d16d12b33092d
Akumar132232 <64557746+Akumar132232@users.noreply.github.com>
2022-07-19 21:54:10 +0530

    Adding Frequency in line in large paragraph

---
+++ HashTableMain.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+public class HashTableMain {
+    public static void main(String[] args) {
+
+        MyHashTable<String, Integer> hashTable = new MyHashTable();
+        String sentence = "Paranoids are not paranoid because they are paranoid but " +
+                "because they keep putting themselves deliberately into paranoid avoidable situations";
+
+
+        String[] sentenceArray = sentence.toLowerCase().split(" ");
+
+        for (String word : sentenceArray) {
+            Integer value = hashTable.get(word);
+
+
+            if( value == null)
+                value = 1;
+            else
+                value = value + 1;
+            hashTable.add(word , value);
+        }
+        System.out.println(hashTable);
+    }
+}
